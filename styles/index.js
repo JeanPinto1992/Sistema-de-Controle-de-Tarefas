021bb88d8607a310041c238d850da435364c64dc
JeanPinto1992 <sanchesjean@gmail.com>
2025-08-19 15:30:11 -0300

    feat: add monthly sector reports

---
+++ styles/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Button = ({ children, ...props }) => <button {...props}>{children}</button>;
+export const Card = ({ children, ...props }) => <div {...props}>{children}</div>;
+export const Input = ({ as = 'input', ...props }) => {
+  const Component = as === 'textarea' ? 'textarea' : as === 'select' ? 'select' : 'input';
+  return <Component {...props} />;
+};
+export const Title = ({ level = 1, children, ...props }) => {
+  const Tag = `h${level}`;
+  return <Tag {...props}>{children}</Tag>;
+};
+export const Form = ({ children, ...props }) => <form {...props}>{children}</form>;
+export const FormGroup = ({ children, ...props }) => <div {...props}>{children}</div>;
